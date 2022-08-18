30421d8da407140268240a0598f97157b24801b4
Alberto <albertoparedes2196@gmail.com>
2022-08-18 15:13:33 +0200

    rama_master chanchanchan

--- src/paquete2/Secundario.java
+++ src/paquete2/Secundario.java
@@ -4,6 +4,6 @@ public class Secundario {
     public static void main(String[] args) {
         
         System.out.println("Emplezando la clase Secundaria");
-
+        System.out.println("chan chan chan");
     }
 }
